2bfacfddd9e64522fefc08a61412b1e4fd75a049
LG29878 <grajalesliliana@johndeere.com>
2021-01-25 21:26:48 -0500

    Commit 2: Tip Calculator express application created, add ?total=110&&tipPercentage=5

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-//in the object 
+
+//Part 1
 const greeting =["Hello stranger",
 { name : "john"    },
 { name : "pierre"  }  ] 
@@ -18,6 +19,18 @@ app.get('/greeting/:name', (req, res) => {
     let name = req.query.name
     res.send(`How are you doing ${name}`);
 })
+
+//Part 2
+// http://localhost:3000/tip/total/tipPercentage/?total=110&&tipPercentage=5
+    app.get('/tip/total/tipPercentage', (req, res) => {
+    let total = req.query.total
+    let percent = req.query.tipPercentage
+    let tipPay = total*percent/100;
+    res.send(`Please pay ${tipPay}`);
+    // res.send(`Please pay ${total} times ${percent}`);
+})
+//Part 3
+
 
 app.listen(3000, () => {
     console.log("Server is listening!!!")
