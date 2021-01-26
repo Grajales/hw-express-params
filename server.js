9dc02e4bc91cc81719cc730848700bbb5bfcde9a
LG29878 <grajalesliliana@johndeere.com>
2021-01-25 21:47:46 -0500

    Commit 3: Magic 8 express application created need to enter magic/question/?question=Will%20I%20Be%20A%20Millionaire

--- server.js
+++ server.js
@@ -27,11 +27,19 @@ app.get('/greeting/:name', (req, res) => {
     let percent = req.query.tipPercentage
     let tipPay = total*percent/100;
     res.send(`Please pay ${tipPay}`);
-    // res.send(`Please pay ${total} times ${percent}`);
-})
+ })
 //Part 3
 
-
+//http://localhost:3000/magic/question/?question=Will%20I%20Be%20A%20Millionaire
+let response =["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+let max = response.length
+app.get('/magic/question', (req, res) => {
+    let question = req.query.question
+    if(question=="Will I Be A Millionaire"){
+    selectNum= Math.floor(Math.random() * Math.floor(max));
+   res.send(`${question} ? response: ${response[selectNum]}`);
+    }
+ })
 app.listen(3000, () => {
     console.log("Server is listening!!!")
 });
